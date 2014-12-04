bd80e6a44f84caf3f1bc813d894c5976c29b8474
renatocrobledo <renatocrobledo@gmail.com>
2014-12-03 19:28:11 -0600

    Añadido registro de buzón

--- Proyecto/sistemaDistribuido/sistema/clienteServidor/modoUsuario/RomeroCarlos/ProcesoServidor.java
+++ Proyecto/sistemaDistribuido/sistema/clienteServidor/modoUsuario/RomeroCarlos/ProcesoServidor.java
@@ -150,7 +150,8 @@ public class ProcesoServidor extends Proceso{
 		byte[] respServer;
 		
 		Nucleo.registrarServidor(ROMERO_FILE_SERVER, dameID());
-
+		imprimeln("Registrando buzón");
+		Nucleo.nucleo.registrarBuzon(dameID());
 		while(continuar()){
 			imprimeln("Invocando a receive()");
 			Nucleo.receive(dameID(),solServer);
@@ -160,6 +161,9 @@ public class ProcesoServidor extends Proceso{
 			Pausador.pausa(1000);  //sin esta línea es posible que Servidor solicite send antes que Cliente solicite receive
 			Nucleo.send(dest,respServer);
 		}
+		
+		imprimeln("deregistrando buzón");
+		Nucleo.nucleo.deregistrarBuzon(dameID());
 		
 		Nucleo.deregistrarServidor(ROMERO_FILE_SERVER, dameID());
 		
